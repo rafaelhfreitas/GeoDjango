1e3aa8d37bc876e737220ebcb2605b33080da971
Rafael <oraffaudi@gmail.com>
2019-05-24 23:55:06 -0300

    WebApp location-based to list 6 nearts shopplaces from a location fixed (Uberlândia) / With Django / GeoDjango / QGIS / Postgres / PostgreGIS

---
+++ nearbyshops/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import generic
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
+
+
+longitude = -48.2622
+latitude = -18.9113
+
+user_location = Point(longitude, latitude, srid=4326)
+
+# Create your views here.
+class Home(generic.ListView):
+    model = Shop
+    context_object_name = "shops"
+    queryset = Shop.objects.annotate(distance=Distance("location", user_location)).order_by("distance")[0:6]
+    template_name = "shops/index.html"
